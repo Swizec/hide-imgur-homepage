7e932190b390399030df192418603c40233a7007
Swizec Teller <swizec@swizec.com>
2018-03-11 15:46:54 -0700

    Replace image grid with break taking advice

--- hide-imgur.js
+++ hide-imgur.js
@@ -1,3 +1,12 @@
+const advice = [
+    "Do some stretches",
+    "Focus on your breathing for 1 minute",
+    "Do 10 pushups",
+    "Do 10 squats",
+    "Walk around for 5 minutes",
+    "Gaze through the window to rest your eyes"
+];
+
 function hideElement(query) {
     let el = document.querySelector(query);
     if (el) {
@@ -15,7 +24,32 @@ function hideAsync(query) {
         setTimeout(function() {
             hideAsync(query);
         }, 1000);
+    } else {
+        putUpSomeContent();
     }
+}
+
+function putUpSomeContent() {
+    const N = document.querySelectorAll(".post").length,
+        sorting = document.querySelector(".sentence-sorting");
+
+    let div = document.querySelector(".hide-imgur-content");
+
+    if (!div) {
+        div = document.createElement("div");
+        div.className = "hide-imgur-content";
+        document.querySelector("#content").insertBefore(div, sorting);
+    }
+
+    div.innerHTML = `
+    <h1>HideImgurHomepage saved you from ${N} distracting images.</h1>
+    <h1>You're welcome!</h1>
+    <a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif"></a>
+    <p>Now, you obviously came here because you need a break.</p>
+    <p><big>${advice[Math.floor(Math.random() * advice.length)]}</big></p>
+    <p>Feel better? Get back to work!</p>
+    <p><small></small></p>
+    `;
 }
 
 // Go through dangerouns elements and hide them
@@ -23,3 +57,5 @@ function hideAsync(query) {
     hideElement
 );
 hideAsync("#extended-imagelist");
+
+putUpSomeContent();
